565fddc5b2c27172c0f73aee38d45226bbe834cb
viktor <sterilework@gmail.com>
2025-03-16 17:39:40 +0300

    feat: add error styling, localize result

--- src/lr_01/scripts/app.js
+++ src/lr_01/scripts/app.js
@@ -4,18 +4,24 @@ document.form_novalidate.addEventListener('submit', (evt) => {
     evt.preventDefault(); // Prevent form submission
 
     const sides = [evt.target.a.value, evt.target.b.value, evt.target.c.value];
-    const output = evt.target.result;
+    const output = evt.target.form_out
+    const result = evt.target.result;
+    const legend = evt.target.form_out.querySelector('legend');
 
     try {
         const triangle = new Triangle([sides[0], sides[1], sides[2]]);
-        const kind = Triangle.kinds.get(triangle.kind).en;
-        output.textContent = `The triangle is ${kind}.`;
+        const kind = Triangle.kinds.get(triangle.kind).ru;
+        legend.textContent = "Результат";
+        result.textContent = `Треугольник ${kind}.`;
+        output.classList.remove("state_error");
     }
     catch (err) {
-        output.textContent = `Error: ${err.message}`;
+        legend.textContent = "Ошибка";
+        result.textContent = `Ошибка: ${err.message}`;
+        output.classList.add("state_error");
     }
 
-    evt.target.form_out.classList.add("is_on");
+    output.classList.add("is_on");
 });
 
 document.form_novalidate.addEventListener('reset', (evt) => {
